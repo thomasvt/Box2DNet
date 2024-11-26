0794daf9630a54814bfe76ea80a81680ae06ee1e
📺Thomas Vantroyen <thomas.vantroyen@gmail.com>
2024-11-26 11:22:20 +0100

    b2XXXId structs: added Equatable and operators == and !=

---
+++ Box2dNet/Interop/B2Api_CastExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Box2dNet.Interop
+{
+    public static partial class B2Api
+    {
+        /// <summary>
+        /// Used with b2World_CastRayClosest_DotNet.
+        /// </summary>
+        public class CastRayClosestFilter
+        {
+            public b2QueryFilter QueryFilter { get; }
+            public Func<b2ShapeId, bool> Predicate { get; }
+
+            internal b2ShapeId? ShapeId = default;
+            internal float Fraction = default;
+            internal bool Hit = false;
+            internal Vector2 Point = default;
+            internal Vector2 Normal = default;
+
+            public CastRayClosestFilter(b2QueryFilter queryFilter, Func<b2ShapeId, bool> predicate)
+            {
+                QueryFilter = queryFilter;
+                Predicate = predicate;
+            }
+
+            public float CastCallback(b2ShapeId shapeId, Vector2 point, Vector2 normal, float fraction, IntPtr /* void* */ context)
+            {
+                if (!Predicate(shapeId)) 
+                    return -1; // ignore this hit
+
+                // because we return 'fraction' we only get called here for an even closer hit than we already had.
+
+                // So, store the info about the latest closest hit:
+                ShapeId = shapeId; // (over)write the closest shape yet.
+                Hit = true;
+                Fraction = fraction;
+                Point = point;
+                Normal = normal;
+
+                return fraction; // only callback again for an even closer shape
+            }
+        }
+
+        /// <summary> 
+        /// .NET extension method similar to b2World_CastRayClosest but with custom filtering.
+        /// </summary>
+        public static b2RayResult b2World_CastRayClosest_DotNet(b2WorldId worldId, Vector2 origin, Vector2 translation, CastRayClosestFilter filter)
+        {
+            // clear previous results in case a single instance is reused for many casts to decrease GC pressure
+            filter.ShapeId = default;
+            filter.Fraction = default;
+            filter.Hit = default;
+            filter.Point = default;
+            filter.Normal = default;
+
+            using var contextHandle = new NativeHandle<CastRayClosestFilter>(filter);
+            var callbackIntPtr = Marshal.GetFunctionPointerForDelegate((b2CastResultFcn)filter.CastCallback);
+            var stats = b2World_CastRay(worldId, origin, translation, filter.QueryFilter, callbackIntPtr, contextHandle.IntPtr);
+            return new b2RayResult
+            {
+                leafVisits = stats.leafVisits,
+                nodeVisits = stats.nodeVisits,
+                fraction = filter.Fraction,
+                hit = filter.Hit,
+                point = filter.Point,
+                normal = filter.Normal
+            };
+        }
+
+        
+    }
+}
--- Box2dNet/Interop/B2Api_StructExtensions.cs
+++ Box2dNet/Interop/B2Api_StructExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-
-namespace Box2dNet.Interop
+﻿namespace Box2dNet.Interop
 {
     public partial struct b2WorldDef
     {
@@ -10,11 +8,36 @@ namespace Box2dNet.Interop
         }
     }
 
-    public partial struct b2WorldId
+    public partial struct b2WorldId : IEquatable<b2WorldId>
     {
         public override string ToString()
         {
             return $"w[{index1}:{revision}]";
+        }
+
+        public bool Equals(b2WorldId other)
+        {
+            return index1 == other.index1 && revision == other.revision;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is b2WorldId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(index1, revision);
+        }
+
+        public static bool operator ==(b2WorldId a, b2WorldId b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(b2WorldId a, b2WorldId b)
+        {
+            return !(a == b);
         }
     }
 
@@ -26,11 +49,36 @@ namespace Box2dNet.Interop
         }
     }
 
-    public partial struct b2BodyId
+    public partial struct b2BodyId : IEquatable<b2BodyId>
     {
         public override string ToString()
         {
             return $"w[{world0}].B[{index1}:{revision}]";
+        }
+
+        public bool Equals(b2BodyId other)
+        {
+            return index1 == other.index1 && world0 == other.world0 && revision == other.revision;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is b2BodyId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(index1, world0, revision);
+        }
+
+        public static bool operator ==(b2BodyId a, b2BodyId b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(b2BodyId a, b2BodyId b)
+        {
+            return !(a == b);
         }
     }
 
@@ -50,11 +98,36 @@ namespace Box2dNet.Interop
         }
     }
 
-    public partial struct b2ShapeId
+    public partial struct b2ShapeId : IEquatable<b2ShapeId>
     {
         public override string ToString()
         {
             return $"W[{world0}].S[{index1}:{revision}]";
+        }
+
+        public bool Equals(b2ShapeId other)
+        {
+            return index1 == other.index1 && world0 == other.world0 && revision == other.revision;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is b2ShapeId other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(index1, world0, revision);
+        }
+
+        public static bool operator ==(b2ShapeId a, b2ShapeId b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(b2ShapeId a, b2ShapeId b)
+        {
+            return !(a == b);
         }
     }
 
